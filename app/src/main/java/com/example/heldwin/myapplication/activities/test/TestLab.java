60bbbb8c9f529545cf3a370e96c43306e8b2444f
unkls <benjamin.leveque97@gmail.com>
2017-10-07 13:12:25 +0200

    ligne ok
    Marche niquel sur 1 ligne

--- app/src/main/java/com/example/heldwin/myapplication/activities/test/TestLab.java
+++ app/src/main/java/com/example/heldwin/myapplication/activities/test/TestLab.java
@@ -55,7 +55,7 @@ public class TestLab  extends AppCompatActivity{
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Case j= (Case) adapterView.getAdapter().getItem(i);
                 Toast.makeText(view.getContext(),j.getPositionX()+" "+j.getPositionY()+ "avec l'image " +j.getImage().getImage(), Toast.LENGTH_SHORT).show();
-                //swipe(j.getPositionX(), j.getPositionY());
+                swipe(j.getPositionX(), j.getPositionY());
             }
         });
         Button btn1 = (Button) findViewById(R.id.btn1);
@@ -63,30 +63,6 @@ public class TestLab  extends AppCompatActivity{
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Case[] tabCase = P.getTabCase();
-                List<Integer> list = new ArrayList<Integer>(); // creer une liste de case à bouger
-                Case cs = tabCase[44];
-                for (int i = 0; i<50; i++) { // on les cherches toutes
-                    if (tabCase[i].getPositionY() == 1) {
-                        list.add(i); // on list les position à changer
-                    }
-                }
-                Case tampon = null;
-                Case tmp=null;
-                tampon = tabCase[list.get(list.size()-1)]; // on met la derniere case dans le tampon
-                int i = list.size()-1; // on prend la derière valeur de la list
-                int p = list.size()-2; // on prend l'avant dernière valeur de la list
-
-                while(p>=0){ //pour pas aller trop loin et taper hors du tableau
-                    Log.e("DEBUG","P à pour valeur "+list.get(p)+" et i a pour valeur "+ list.get(i));
-                    tabCase[list.get(i)]=tabCase[list.get(p)]; // on met la case -1 dans la case -2 (comme expliquer à l'oral)
-                    i--;
-                    p--;
-                }
-                tabCase[1]=tabCase[49]; // à faire à la main
-                tabCase[49]=tampon; // change la case 49 pour la prochaine ligne à pousser
-                adapter.notifyDataSetChanged(); // sert à rafraichir le gridView
-                gridViewPlateau.setAdapter(adapter);// le rafraichis ici
             }
         });
     }
@@ -95,7 +71,7 @@ public class TestLab  extends AppCompatActivity{
         switch (posY){
             case 0:
                 if (posX == 1 ){
-                    pousser(0,1,1);
+                    pousserY();
                 }
                 else if (posX == 3 ){
 
@@ -119,31 +95,39 @@ public class TestLab  extends AppCompatActivity{
     }
 
 //faire bouger à l'envers case tampons prenend dernière case ce qui crée une case vide on remonte avec la case vide puis on met la case 7,0 dans la première
-    public void pousser(int posX, int posY, int cas){
+    public void pousserY(){
 
-        Case[] tabCase = P.getTabCase(); //getTabCase
+        final Case[] tabCase = P.getTabCase();
         List<Integer> list = new ArrayList<Integer>(); // creer une liste de case à bouger
+        Case cs = tabCase[44];
         for (int i = 0; i<50; i++) { // on les cherches toutes
-            if (tabCase[i].getPositionY() == posY) {
+            if (tabCase[i].getPositionY() == 1) {
                 list.add(i); // on list les position à changer
             }
         }
         Case tampon = null;
-        Case tmp=null;
-        tabCase[list.get(list.size()-1)]=tampon; // on met la derniere case dans le tampon
-        tabCase[list.get(list.size()-1)]=null;
+        tampon = tabCase[list.get(list.size()-1)]; // on met la derniere case dans le tampon
+        int i = list.size()-1; // on prend la derière valeur de la list
+        int p = list.size()-2; // on prend l'avant dernière valeur de la list
 
-        for (int i=1; i<list.size();i++){
+        while(p>=0){ //pour pas aller trop loin et taper hors du tableau
+            Log.e("DEBUG","P à pour valeur "+list.get(p)+" et i a pour valeur "+ list.get(i));
+            tabCase[list.get(i)]=tabCase[list.get(p)]; // on met la case -1 dans la case -2 (comme expliquer à l'oral)
+            tabCase[list.get(i)].setPositionX(i);
+            i--;
+            p--;
+            Log.e("DEBUG"," i à pour X " +tabCase[list.get(i)].getPositionX());
 
-            tabCase[list.get(list.size()-(i))].setPositionY(tabCase[list.get(list.size()-(1+i))].getPositionY());
-            tabCase[list.get(list.size()-(i))].setPositionX(tabCase[list.get(list.size()-(1+i))].getPositionX());
-            tabCase[list.get(list.size()-(i))] = tabCase[list.get(list.size()-(1+i))] ; // la case -2 va dans la -1
         }
+        tabCase[list.get(i)]=tabCase[49]; // à faire à la main
+        tabCase[list.get(i)].setPositionY(1);
+        tabCase[list.get(i)].setPositionX(0);
 
-        tabCase[list.get(0)] = tabCase[50]; // on met la premiere avec la case qui va etre glisser
-        tabCase[50]= tampon;//on met le tampon dans la prochaine case à bouger
-        adapter.notifyDataSetChanged();
-        gridViewPlateau.setAdapter(adapter);
+        tabCase[49]=tampon; // change la case 49 pour la prochaine ligne à pousser
+        tabCase[49].setPositionX(7);
+        tabCase[49].setPositionY(0);
+        adapter.notifyDataSetChanged(); // sert à rafraichir le gridView
+        gridViewPlateau.setAdapter(adapter);// le rafraichis ici
 
     }
 }
